d0699a1f62afb1e2cca9d366729a0618fc8c08d9
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-12-13 23:30:01 -0600

    fixed errors

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -5,13 +5,13 @@ const butInstall = document.getElementById("buttonInstall");
 window.addEventListener("beforeinstallprompt", (event) => {
   // Store the triggered events
   window.deferredPrompt = event;
-
   // Remove the hidden class from the button.
   butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
     return;
   }
